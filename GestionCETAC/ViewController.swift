97a3d0e0a61736037f078dc248b8595b1ef8dcf5
Agustin Abreu <a01653126@itesm.mx>
2021-09-22 15:18:48 -0700

    Initial Commit

---
+++ GestionCETAC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestionCETAC
+//
+//  Created by user192935 on 9/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
